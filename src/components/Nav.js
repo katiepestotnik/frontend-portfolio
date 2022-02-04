e7be2c1b2f2e660e652f47264009ace37b7d3ef9
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-02-03 17:37:45 -0700

    add favicon and styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,21 @@ import { Link } from 'react-router-dom';
 
 function Nav(props) {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <Link to="/"class="navbar-brand" >Katie's Portfolio</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <>
+        <nav class="navbar navbar-expand-xl navbar-light main-color">
+  <div class="nav-main" >Katie's Portfolio</div>
+                <button style={{backgroundColor: 'white'} }class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <Link to="/"class="nav-item nav-link active">Home <span class="sr-only">(current)</span></Link>
-      <Link to="/projects"class="nav-item nav-link">Projects</Link>
-      <Link to="/about"class="nav-item nav-link" >About Katie</Link>
+                    <div style={{ justifyContent: 'flex-end', alignItems: 'center'}}class="navbar-nav container">
+      <Link to="/"class="nav-item nav-items">Home </Link>
+      <Link to="/projects"class="nav-item nav-items">Projects</Link>
+      <Link to="/about"class="nav-item nav-items" >About</Link>
     </div>
   </div>
-</nav>
+            </nav>
+            </>
     );
 }
 
